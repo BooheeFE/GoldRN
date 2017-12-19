cd19859fc697864a98d058ec11e2b7d524a3d61e
simbawu <code@simbawu.com>
2017-12-19 19:47:13 +0800

    tit组件修改

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -30,23 +30,29 @@ export default class NavigationBar extends Component<{}> {
   render(){
 
     return (
-        <Text style={[styles.title, isiOS ? {marginTop: 16}:'']}>
-          {this.props.title || '金问号'}
-        </Text>
+        <View style={[styles.titleBox, isiOS ? {marginTop: 16}:'']}>
+          <Text style={styles.title}>{this.props.title || '金问号'}</Text>
+        </View>
     );
   }
 
 }
 
 const styles = StyleSheet.create({
-  title: {
+  titleBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent: 'center',
     margin: 0,
     height: 48,
-    lineHeight: 48,
-    textAlign: 'center',
+    backgroundColor: '#fff'
+  },
+  title: {
+    margin: 0,
     fontSize: 17,
     color: '#333',
-    backgroundColor: '#fff',
+    textAlign: 'center'
   }
 });
 
